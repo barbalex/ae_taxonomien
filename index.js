f0fe8115d346f6bf13a1d4f095cb60a26e8c4e08
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-11-21 22:13:29 +0100

    optimize code

--- index.js
+++ index.js
@@ -1,93 +1,83 @@
 'use strict'
 
-var couchPass = require('./couchPass.json')
-var url = 'http://' + couchPass.user + ':' + couchPass.pass + '@127.0.0.1:5984'
-var nano = require('nano')(url)
-var adb = nano.db.use('artendb')
-var _ = require('lodash')
+const couchPass = require('./couchPass.json')
+const url = `http://${couchPass.user}:${couchPass.pass}@127.0.0.1:5984`
+const nano = require('nano')(url)
+const adb = nano.db.use('artendb')
+const _ = require('lodash')
 
 adb.view('artendb', 'objekte', {
   'include_docs': true
-}, function (err, body) {
-  if (!err) {
-    // extract object docs from result
-    var docsArray = body.rows.map(function (row) {
-      return row.doc
-    })
-    adb.view('artendb', 'dsMetadataNachDsName', {
-      'include_docs': true
-    }, function (err, body) {
-      if (!err) {
-        // extract metadata doc from result
-        var taxMetadataArray = body.rows.map(function (row) {
-          return row.doc
-        })
-        var taxMetadata = _.indexBy(taxMetadataArray, 'Name')
+}, (error, body) => {
+  if (error) return console.log(error)
+  adb.view('artendb', 'dsMetadataNachDsName', {
+    'include_docs': true
+  }, function (err, body) {
+    if (err) return console.log(err)
+    // extract metadata doc from result
+    const taxMetadataArray = body.rows.map((row) => row.doc)
+    const taxMetadata = _.indexBy(taxMetadataArray, 'Name')
 
-        var docs = []
+    let docs = []
 
-        _.each(docsArray, function (doc) {
-          if (doc.Gruppe && doc.Taxonomie && doc.Taxonomie.Eigenschaften) {
-            var neueTax = _.cloneDeep(doc.Taxonomie)
-            if (doc.Gruppe === 'Lebensräume' && neueTax.Eigenschaften.Parent) {
-              if (!neueTax.Eigenschaften.Parent) {
-                console.log('lr ' + doc._id + ' hat keinen Parent')
-              } else if (!neueTax.Eigenschaften.Hierarchie) {
-                console.log('lr ' + doc._id + ' hat keine Hierarchie')
-              } else {
-                delete neueTax.Eigenschaften.Parent
-              }
-            } else {
-              // this is a species
-              var hierarchie = []
-              var metaData = taxMetadata[doc.Taxonomie.Name]
-              if (metaData) {
-                if (metaData.HierarchieFelder) {
-                  _.forEach(metaData.HierarchieFelder, function (feld, index) {
-                    if (neueTax.Eigenschaften[feld]) {
-                      if (index + 1 === metaData.HierarchieFelder.length) {
-                        hierarchie.push({
-                          'Name': neueTax.Eigenschaften['Artname vollständig'],
-                          'GUID': doc._id
-                        })
-                      } else {
-                        hierarchie.push({
-                          'Name': neueTax.Eigenschaften[feld]
-                        })
-                      }
-                    }
-                  })
-                  neueTax.Eigenschaften.Hierarchie = hierarchie
+    // loop through docs
+    body.rows.forEach((row, index) => {
+      const doc = row.doc
+      if (doc.Gruppe && doc.Taxonomie && doc.Taxonomie.Eigenschaften) {
+        let neueTax = _.cloneDeep(doc.Taxonomie)
+        if (doc.Gruppe === 'Lebensräume' && neueTax.Eigenschaften.Parent) {
+          if (!neueTax.Eigenschaften.Parent) {
+            console.log('lr ' + doc._id + ' hat keinen Parent')
+          } else if (!neueTax.Eigenschaften.Hierarchie) {
+            console.log('lr ' + doc._id + ' hat keine Hierarchie')
+          } else {
+            delete neueTax.Eigenschaften.Parent
+          }
+        } else {
+          // this is a species
+          let hierarchie = []
+          const metaData = taxMetadata[doc.Taxonomie.Name]
+          if (metaData) {
+            if (metaData.HierarchieFelder) {
+              _.forEach(metaData.HierarchieFelder, (feld, index) => {
+                if (neueTax.Eigenschaften[feld]) {
+                  if (index + 1 === metaData.HierarchieFelder.length) {
+                    hierarchie.push({
+                      'Name': neueTax.Eigenschaften['Artname vollständig'],
+                      'GUID': doc._id
+                    })
+                  } else {
+                    hierarchie.push({
+                      'Name': neueTax.Eigenschaften[feld]
+                    })
+                  }
                 }
-              } else {
-                console.log('doc ' + doc._id + ' has no metaData')
-              }
+              })
+              neueTax.Eigenschaften.Hierarchie = hierarchie
             }
-            neueTax.Eigenschaften = neueTax.Eigenschaften
-            doc.Taxonomien = [neueTax]
-            docs.push(doc)
           } else {
-            console.log('doc ' + doc._id + ' has no Gruppe or no Taxonomie')
+            console.log('doc ' + doc._id + ' has no metaData')
           }
-        })
-
-        // bulk-Format aufbauen
-        var bulk = {}
-        bulk.docs = docs
-
-        // alle Updates in einem mal durchführen
-        adb.bulk(bulk, function (error, result) {
-          if (error) {
-            console.log('error after bulk:', error)
-          } else {
-            console.log(docs.length + ' Objekte aktualisiert. Result from bulk:', result)
-          }
-        })
+        }
+        neueTax.Eigenschaften = neueTax.Eigenschaften
+        doc.Taxonomien = [neueTax]
+        docs.push(doc)
       } else {
-        console.log('err: ', err)
+        console.log('doc ' + doc._id + ' has no Gruppe or no Taxonomie')
       }
     })
-  } else {
-    console.log('err: ', err)
-  }
+
+    // bulk-Format aufbauen
+    var bulk = {}
+    bulk.docs = docs
+
+    // alle Updates in einem mal durchführen
+    adb.bulk(bulk, function (error, result) {
+      if (error) {
+        console.log('error after bulk:', error)
+      } else {
+        console.log(docs.length + ' Objekte aktualisiert. Result from bulk:', result)
+      }
+    })
+  })
 })
